7389abcfbb17ec08019fc8d10ac158cf1fc2be25
DB <84276315+atari1040st@users.noreply.github.com>
2021-05-22 19:12:34 -0400

    update compile script

--- azure_script/compile_and_config.sh
+++ azure_script/compile_and_config.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 
-cd ..
-rm -rf xmrig/
-git clone https://github.com/xmrig/xmrig.git
-cd xmrig
-git checkout v5.5.3
-mkdir build
-cd build
-cmake ..
-make 
-cd ..
-cd ..
+SHA256SUM="baf3f5489aff8b7f26452f458a809981ca16a5529c798373b63f3878a3943932  xmrig-6.12.1-bionic-x64.tar.gz"
 
+cd ..
+echo $SHA256SUM > SHA256SUM
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-bionic-x64.tar.gz
+sha256sum -c SHA256SUM --quiet
+
+if [ $? -eq "0" ]; then
+  tar zxf xmrig-6.12.1-bionic-x64.tar.gz
+  ln -s xmrig-6.12.1 xmrig
+  exit 0
+fi
+
+exit 1
